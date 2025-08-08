378f7551ef8767c8994e3028946c1fe48b1c631e
Andreas Gohr <andi@splitbrain.org>
2025-08-08 17:56:43 +0200

    improved firewall config
    added comments, let docker containers always access host ports without
    having to open them to the world

--- modules/networking.nix
+++ modules/networking.nix
@@ -17,10 +17,27 @@
     defaultGateway = "192.168.1.1";
     nameservers = [ "192.168.1.1" ];
 
+    nftables.enable = true; # new nft instead of iptables
+
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 5001 22000];
-      allowedUDPPorts = [ 51820 22000 21027 ];
+      allowedTCPPorts = [
+        5001
+        22000 # syncthing
+        51413 # bittorrent (forwarded from router)
+      ];
+      allowedUDPPorts = [
+        51820 # wireguard (forwarded from router)
+        22000 # syncthing
+        21027 # syncthing
+        51413 # bittorrent (forwarded from router)
+        1900  # gerbera UPNP
+      ];
+
+      # always let docker images access host services
+      extraInputRules = ''
+        ip saddr 172.16.0.0/12 accept
+      '';
     };
   };
 
